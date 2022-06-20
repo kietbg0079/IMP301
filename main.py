84ff6cf2413e750950e6f221c670e9131ee0fee5
Kiet Ngo <kietna4@fosft.com.vn>
2022-06-20 16:08:49 +0700

    add morphological

--- main.py
+++ main.py
@@ -9,18 +9,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    img = cv2.imread('data/10.2/img4.png', 0)
+    img = cv2.imread('data/10.2/test.jpg')
 
-    mh = marr_hildreth(img)
+    morp = morphological(img)
 
-    # gaus_fil = mh.gaus_dis()
-    #
-    # conv_img = convolution2d(img).convolution(gaus_fil)
-    #
-    # lapla = convolution2d(conv_img).convolution(KERNEL['laplacian'])
-    #
-    # zec = smooth_threshold(img).threshold(lapla, 0.04)
+    out_img = morp.morphol()
 
-    multiplot("Marr and Hildreth", {"origin" : img, "dog" : mh.DoG(2, 4, 1, 5)})
+    multiplot("Marr and Hildreth", {"origin" : img, "morphological" : out_img})
 
 
--- src/filter.py
+++ src/filter.py
@@ -346,3 +346,55 @@ class Split_Merge_Segmented:
 
         multiplot("Split and merge segmentation", {
                   'input image': origin, "input gray": img_gray, "segmented image": segemented_img})
+
+
+
+
+
+
+
+class morphological():
+    """
+        Image segmentation using morphological wartersheed method
+
+        Args:
+            image (RGB channel): Input image
+        Returns:
+            image : with the object surrounded by a boundary
+    """
+    def __init__(self, img):
+        self.img = img
+
+
+    def morphol(self):
+
+        gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
+
+        ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+        # noise removal
+        kernel = np.ones((3, 3), np.uint8)
+        closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+
+        # background area
+        sure_bg = cv2.dilate(closing, kernel, iterations=1)
+
+        # Finding foreground area
+        dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 5)
+        ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg, sure_fg)
+
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(sure_fg)
+
+        # Applying watershed algorithm and marking the regions segmented
+        markers = markers + 1
+        markers[unknown == 255] = 0
+        markers = cv2.watershed(self.img, markers)
+
+        self.img[markers == -1] = [255, 0, 0]
+
+        return self.img
